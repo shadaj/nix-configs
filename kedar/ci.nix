d0bc3bc4a0b192c1239bd7b3cbf10a87b45769fd
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-07-07 13:08:59 -0700

    Attempt to fix reboot hangs, upgrade CI, and reduce restic downloads

--- kedar/ci.nix
+++ kedar/ci.nix
@@ -12,7 +12,7 @@ in {
       DRONE_GITHUB_CLIENT_ID = secrets.client-id;
       DRONE_GITHUB_CLIENT_SECRET = secrets.client-secret;
       DRONE_RPC_SECRET = secrets.rpc-secret;
-      DRONE_SERVER_HOST = "kedar.local";
+      DRONE_SERVER_HOST = "***REMOVED***";
       DRONE_SERVER_PROTO = "http";
       DRONE_USER_CREATE = "username:shadaj,admin:true";
     };
@@ -26,7 +26,7 @@ in {
     image = "drone/drone-runner-docker:1";
     environment = {
       DRONE_RPC_PROTO = "http";
-      DRONE_RPC_HOST = "kedar";
+      DRONE_RPC_HOST = "localhost";
       DRONE_RPC_SECRET = "abcd1234";
       DRONE_RUNNER_CAPACITY = "2";
       DRONE_RUNNER_NAME = "kedar-runner";
