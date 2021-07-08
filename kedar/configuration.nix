00862bef15c9d877424aef7339961dd177bda47a
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-07-07 21:15:18 -0700

    Bump to ZFS 2.1 stable and use beta NVIDIA driver to fix shutdown hangs

--- kedar/configuration.nix
+++ kedar/configuration.nix
@@ -52,7 +52,6 @@ in {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.enableUnstable = true; # until 5.12 support in stable
   boot.loader.systemd-boot.consoleMode = "max";
 
   systemd.extraConfig = ''
@@ -92,6 +91,19 @@ in {
     allowUnfree = true;
     packageOverrides = super: let self = super.pkgs; in {
       tailscale = unstable.tailscale;
+      linuxPackages_5_12 = unstable.linuxPackages_5_12.extend (linuxSelf: linuxSuper:
+      let
+        generic = args: linuxSelf.callPackage (import <nixos-unstable/pkgs/os-specific/linux/nvidia-x11/generic.nix> args) { };
+      in
+      {
+        nvidiaPackages.stable = generic {
+          version = "465.31";
+          sha256_64bit = "1nic4xvx5ihqijm2f57kg97nyh4nmfk7p3ikkh266n1kr40x0230";
+          settingsSha256 = "1wgnag0d5g82vcmpwgz3rsx6w6q6yf48bsmi268xxzn62xgwfz6z";
+          persistencedSha256 = "11n26vnzp33fqmyr7dj04fb0mirydh9ylf34x8vsz0xi6fmd1gyn";
+        };
+      });
+      zfs = unstable.zfs;
     };
   };
 
--- kedar/vm/revert.sh
+++ kedar/vm/revert.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
-# set -x
 
 # Unload VFIO-PCI Kernel Driver
-modprobe -r vfio-pci
-modprobe -r vfio_iommu_type1
 modprobe -r vfio
+modprobe -r vfio_iommu_type1
+modprobe -r vfio-pci
 
 # Re-Bind GPU to Nvidia Driver
+virsh nodedev-reattach pci_0000_0c_00_4
+
 virsh nodedev-reattach pci_0000_0a_00_3
 virsh nodedev-reattach pci_0000_0a_00_2
 virsh nodedev-reattach pci_0000_0a_00_1
 virsh nodedev-reattach pci_0000_0a_00_0
 
-virsh nodedev-reattach pci_0000_0c_00_4
-
-# Rebind VT consoles
-echo 1 > /sys/class/vtconsole/vtcon0/bind
-echo 1 > /sys/class/vtconsole/vtcon1/bind
+modprobe ipmi_msghandler
+modprobe ipmi_devintf
+modprobe nvidia
+modprobe nvidia_uvm
+modprobe nvidia_modeset
+modprobe nvidia_drm
 
 nvidia-xconfig --query-gpu-info > /dev/null 2>&1
 echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
 
-modprobe nvidia_drm
-modprobe nvidia_modeset
-modprobe nvidia_uvm
-modprobe nvidia
-modprobe ipmi_devintf
-modprobe ipmi_msghandler
-modprobe snd_hda_intel
+# Rebind VT consoles
+echo 1 > /sys/class/vtconsole/vtcon0/bind
+echo 1 > /sys/class/vtconsole/vtcon1/bind
 
 # Restart Display Manager
 systemctl start display-manager.service
