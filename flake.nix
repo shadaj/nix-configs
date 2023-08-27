d949a1bae12fafc5915191ffeaebc1ed147fc47f
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-08-27 14:33:14 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           config = {
             allowUnfree = true;
             permittedInsecurePackages = [
-              "nodejs-16.20.1"
+              "nodejs-16.20.2"
             ];
           };
         };
